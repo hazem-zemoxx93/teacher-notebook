035dacd1ee1d850868bd02dd5f9a7a5bb19d89d4
hazem-zemoxx93 <hazem.zemoxx93@gmail.com>
2026-07-02 17:16:15 +0300

    Add all state management providers (Theme, Group, Student, Attendance, Payment, App)

--- lib/providers/app_provider.dart
+++ lib/providers/app_provider.dart
@@ -1,6 +1,4 @@
-import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:teacher_notebook/database/database_helper.dart';
 
 class AppProvider extends ChangeNotifier {
   bool _isBackupLoading = false;
@@ -10,15 +8,4 @@ class AppProvider extends ChangeNotifier {
   bool get isBackupLoading => _isBackupLoading;
   String? get backupError => _backupError;
   String? get backupSuccess => _backupSuccess;
-
-  Future<void> deleteAllData() async {
-    try {
-      await DatabaseHelper.instance.deleteAllData();
-      _backupSuccess = 'تم حذف جميع البيانات بنجاح';
-      notifyListeners();
-    } catch (e) {
-      _backupError = e.toString();
-      notifyListeners();
-    }
-  }
 }
